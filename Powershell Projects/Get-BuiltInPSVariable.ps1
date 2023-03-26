f38234a0fa37ba4f193340c7ee46ad3222a0235d
Harish772 <40069272+Harish772@users.noreply.github.com>
2023-03-26 09:59:36 -0400

    Add files via upload

---
+++ Powershell Projects/Get-BuiltInPSVariable.ps1
@@ -0,0 +1,16 @@
+﻿#With PSversion 5 you can eliminate this function and use: 
+    #get-childitem variable:
+
+function Get-BuiltInPSVariable($Name='*')
+{
+  # create a new PowerShell
+  $ps = [PowerShell]::Create()
+  # get all variables inside of it
+  $null = $ps.AddScript('$null=$host;Get-Variable') 
+  $ps.Invoke() |
+    Where-Object Name -like $Name
+  # dispose new PowerShell
+  $ps.Runspace.Close()
+  $ps.Dispose()
+}
+Get-BuiltInPSVariable($Name='*')
